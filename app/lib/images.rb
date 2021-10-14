c52b9bef53348e9e9f8b94c44d51ea5c39c49675
TernaryCat <sad.coder@yandex.ru>
2021-10-15 01:46:07 +0600

    add color schemes

--- app/lib/images.rb
+++ app/lib/images.rb
@@ -22,7 +22,6 @@ module Images
     %w(#FBE7C6 #B4F8C8 #A0E7E5 #FFAEBC),
     %w(#E7D2CC #B9B7BD #868B8E #EEEDE7),
     %w(#05445E #189AB4 #75E6DA #D4F1F4),
-    %w(#145DA0 #0C2D48 #2E8BC0 #B1D4E0),
     %w(#B99095 #FCB5AC #B5E5CF #3D5B59),
     %w(#FFC2C7 #B6E5D8 #FBE5C8 #8FDDE7),
     %w(#F8EA8C #A4E8E0 #4CD7D0 #E1C340),
@@ -35,7 +34,25 @@ module Images
     %w(#EFEBE0 #FB8DA0 #FB6B90 #FB4570),
     %w(#F8C0C8 #EFE7D3 #D3BBDD #ECE3F0),
     %w(#DACAC4 #F7C9B6 #DBC39A #DAE7DD),
-    %w(#ACEEF3 #FF7077 #FFE9E4 #FFB067)
+    %w(#ACEEF3 #FF7077 #FFE9E4 #FFB067),
+    %w(#e7ddd2 #cc9764 #934f28 #cb401b #30360f),
+    %w(#a0b3a8 #c6a78f #ecc8c9 #bb99b7 #e48826),
+    %w(#909cac #cec3c8 #ecdab9 #efca66 #a47053),
+    %w(#93b0d0 #dee8ec #eacc97 #a36c22 #6f2205),
+    %w(#42281c #a35233 #d6834f #e6ae74 #f6d2ac),
+    %w(#9db802 #025b0e #fd7c84 #cfcdcb #f0eeef),
+    %w(#65b2c6 #c0cccc #d6c2bc #d57276 #d73d6c),
+    %w(#6f6c9e #bb7db2 #d7cbd4 #e3c09b #bfd2de),
+    %w(#eadbd7 #eae1ef #b8afc9 #02a0da #97cded),
+    %w(#204429 #636f44 #dcaab1 #c8777b #a63d4c),
+    %w(#975536 #e5a484 #dddde2 #a2d4df #528c83),
+    %w(#958948 #e7c8ba #dbb5bb #aa646c #7f5b5a),
+    %w(#21574a #628272 #9ca18d #d39b75 #dab7a3),
+    %w(#57394a #b2481b #eab586 #e0c2c0 #dbb0a0),
+    %w(#dc6d02 #fcb500 #edc596 #fd9a7e #c43d16),
+    %w(#f2b9cc #523637 #bb8918 #e1dc6b #a1ae25),
+    %w(#ec4f43 #fe7968 #fe948d #ffbdb3 #ffe0db),
+    %w(#d2afae #a5c6b1 #f4c88c #cc883e #5e6464)
   ]
 
   BACKGROUNDS = [
